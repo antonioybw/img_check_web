393c3c0e4d3ce6d0532c10b68d6f5f78280cf4da
Bowen Yang <antonioyangbowen@gmail.com>
2017-10-30 23:09:34 +0000

    save temp the way to match scipy image v2

--- img_detect_server.py
+++ img_detect_server.py
@@ -61,7 +61,7 @@ def find_img_list(detect_img_path=detect_img_path):
 
 
 def save_image_to_static(img_str,save_path,img_id):
-  # item_b64_dec = base64.b64decode(img_str)
+  item_b64_dec = base64.b64decode(img_str)
   # with open(save_path+'new_img_'+img_id+'.png','wb') as imgfile:
   #   imgfile.write(item_b64_dec)
   np_array = numpy.fromstring(item_b64_dec, numpy.uint8) 
